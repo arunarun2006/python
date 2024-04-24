523dcaba130b3a8e9747a2df0729447fa873ddb3
Arun <arun@gmail.com>
2024-04-24 11:56:35 +0530

    doneee

---
+++ area.py
@@ -0,0 +1,21 @@
+import math
+
+def circle(radius):
+    '''
+    To find the area of the circle 
+    circle(radius)
+    radius-enter the radius of the circle
+    '''
+    area=math.pi*radius**2
+    return area
+
+def rectangle(length,breath):
+    '''
+    To find the area of the rectangle
+    rectangle(length,breath)
+    length-enter the length of rectangle
+    breath-enter the breath of the rectangle
+    '''
+
+    area=length*breath
+    return area
